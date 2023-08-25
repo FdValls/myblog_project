3079a6ef570e41162722cc5237cde8afb3a02284
Fdvalls <fernandodanielvalls@gmail.com>
2023-08-25 20:12:57 -0300

    modelando blog

--- myBlog-project/src/components/Menu/BottomNavigation.jsx
+++ myBlog-project/src/components/Menu/BottomNavigation.jsx
@@ -1,55 +1,39 @@
 import * as React from "react";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
-import FolderIcon from "@mui/icons-material/Folder";
-import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import styles from "./BottomNavigation.css";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import home from "../../images/home.png"; // Importa la imagen del icono
 import style from "./style.js";
 
 export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState("recents");
-  const navigate = useNavigate();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleButtonClick = () => {
-    // Aquí puedes agregar la lógica que deseas que ocurra al hacer clic en el botón.
-    console.log("Botón 'Recents' fue clickeado");
-    // alert("Hello World")
-    // navigate(`/home`);
-  };
-
   const iconStyles = {
-    width: "28px", // Ancho del icono
-    height: "28px", // Altura del icono
-    // Otros estilos personalizados que desees aplicar
+    width: "28px", 
+    height: "28px", 
   };
 
   return (
     <header style={style}>
       <BottomNavigation value={value} onChange={handleChange}>
         <BottomNavigationAction
-          // label="Home"
           value="/"
           icon={<img src={home} alt="Custom Icon" style={iconStyles} />}
-          // onClick={handleButtonClick}
           component={Link}
-          to="/home" // Agrega el evento onClick aquí
+          to="/home" 
         />
         <BottomNavigationAction
-          label="Crear"
           value="crear"
           icon={<FavoriteIcon />}
           component={Link}
           to="/crear"
         />
         <BottomNavigationAction
-          label="Posteo"
           value="posteo"
           icon={<LocationOnIcon />}
           component={Link}
--- myBlog-project/src/components/Menu/style.js
+++ myBlog-project/src/components/Menu/style.js
@@ -1,12 +1,12 @@
 const style = {
-  position: 'absolute',
-  top: '0',
-  left: '0',
-  width: '100%',
-  flexDirection: 'row',
-  flexwrap: 'initial',
+  position: "absolute",
+  top: "0",
+  left: "0",
+  width: "100%",
+  flexDirection: "row",
+  flexwrap: "initial",
   gap: "15px",
-  background: "linear-gradient(205deg, #1B1B6F 0%, #551CAD 53%, #CC6262 98%)",    
-  };    
-  
+  background: "linear-gradient(205deg, #1B1B6F 0%, #551CAD 53%, #CC6262 98%)",
+};
+
 export default style;
