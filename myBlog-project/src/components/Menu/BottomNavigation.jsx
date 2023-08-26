84e6464e0af087323882a7579a7f11372cb265a8
Fdvalls <fernandodanielvalls@gmail.com>
2023-08-26 09:25:11 -0300

    estilando

--- myBlog-project/src/components/Menu/BottomNavigation.jsx
+++ myBlog-project/src/components/Menu/BottomNavigation.jsx
@@ -4,7 +4,10 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import home from "../../images/home.png"; // Importa la imagen del icono
+import post from "../../images/post.png"; // Importa la imagen del icono
+import create from "../../images/create.png"; // Importa la imagen del icono
 import style from "./style.js";
+import labelColor from "./BottomNavStyles.css";
 
 export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState("recents");
@@ -19,23 +22,26 @@ export default function LabelBottomNavigation() {
   };
 
   return (
-    <header style={style}>
-      <BottomNavigation value={value} onChange={handleChange}>
+    <header >
+      <BottomNavigation style={style} value={value} onChange={handleChange}>
         <BottomNavigationAction
+          label={<span className="labelColor">Home</span>}
           value="/"
           icon={<img src={home} alt="Custom Icon" style={iconStyles} />}
           component={Link}
           to="/home" 
         />
         <BottomNavigationAction
+         label={<span className="labelColor">Crear</span>}
           value="crear"
-          icon={<FavoriteIcon />}
+          icon={<img src={create} alt="Custom Icon" style={iconStyles} />}
           component={Link}
           to="/crear"
         />
         <BottomNavigationAction
+         label={<span className="labelColor">Posteo</span>}
           value="posteo"
-          icon={<LocationOnIcon />}
+          icon={<img src={post} alt="Custom Icon" style={iconStyles} />}
           component={Link}
           to="/posteo"
         />
--- myBlog-project/src/components/Menu/style.js
+++ myBlog-project/src/components/Menu/style.js
@@ -1,12 +1,14 @@
 const style = {
   position: "absolute",
+  // background: "linear-gradient(205deg, #1B1B6F 0%, #551CAD 53%, #CC6262 98%)",
+  background: "linear-gradient(90deg, rgba(17,18,17,0.16290266106442575) 0%, rgba(9,115,121,0.8491771708683473) 66%, rgba(0,212,255,1) 100%)",
   top: "0",
   left: "0",
   width: "100%",
   flexDirection: "row",
+  justifyContent: "end",
   flexwrap: "initial",
-  gap: "15px",
-  background: "linear-gradient(205deg, #1B1B6F 0%, #551CAD 53%, #CC6262 98%)",
+  gap: "100px",
 };
 
 export default style;
